682d4cdd67635df6f4d0b44ccf40b050c2231b58
Marvalero <mariavalerocam@gmail.com>
2021-01-30 23:45:19 +0100

    Style Menu

--- prometeo/src/Header/Header.js
+++ prometeo/src/Header/Header.js
@@ -1,28 +1,85 @@
-import { AppBar, Toolbar, Typography, Button, withStyles, IconButton } from '@material-ui/core';
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	Button,
+	withStyles,
+	IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import React, { Component } from 'react';
+import './Header.css'
+import { Link as RouterLink } from "react-router-dom";
 
 const styles = theme => ({
   root: {
     backgroundColor: "DarkOrange"
-  }
+  },
+  logo: {
+    fontFamily: "Work Sans, sans-serif",
+    fontWeight: 600,
+    color: "#FFFEFE",
+    textAlign: "left",
+  },
+   menuButton: {
+      fontFamily: "Open Sans, sans-serif",
+      fontWeight: 700,
+      size: "18px",
+      marginLeft: "38px",
+   },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
 });
 
-class Header extends Component {
-	classes = {
-		title: "PRometeooo",
-		menuButton: "Blah"
-	}
+class Header extends Component {	
+	headersData = [
+	  {
+	    label: "Blog",
+	    href: "/blog",
+	  },
+	  {
+	    label: "Añadir my aplicación",
+	    href: "/oferentes",
+	  },
+	  {
+	    label: "Hosteleria",
+	    href: "/hosteleros",
+	  },
+	  {
+	    label: "Entrar",
+	    href: "/login",
+	  },
+	];
+
+	getMenuButtons = () => {
+	    return this.headersData.map(({ label, href }) => {
+	      return (
+		<Button
+		  {...{
+		    key: label,
+		    color: "inherit",
+		    to: href,
+		    component: RouterLink,
+		    className: this.props["className"]
+		  }}
+		>
+		  {label}
+		</Button>
+	      );
+	    });
+	  };
+	  
 	render() {
 		const { classes } = this.props;
 		return(
 			<AppBar position="static" className={classes.root}>
-			  <Toolbar>
+			  <Toolbar className={classes.toolbar}>
 				<IconButton edge="start" color="inherit" aria-label="menu">
 					<MenuIcon />
 				</IconButton>
-				<Typography variant="h6">Prometeo</Typography>
-				<Button color="inherit">Login</Button>
+				<Typography variant="h6" component="h1" className={classes.logo}>Prometeo</Typography>
+				  <div className={classes.menuButton}>{this.getMenuButtons()}</div>
 			  </Toolbar>
 			</AppBar>
 		)
