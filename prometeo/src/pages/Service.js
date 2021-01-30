395af9301f623a34024bb293e56c5c4a71d56c67
Marvalero <mariavalerocam@gmail.com>
2021-01-30 17:38:35 +0100

    New Service file

---
+++ prometeo/src/pages/Service.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class Service extends Component {
+    render() {
+      return (
+        <div className="service">
+            <h1>Bla</h1>
+            <p>Bla</p>
+        </div>
+      )
+    }
+}
+
+export default Service
--- prometeo/src/pages/index.js
+++ prometeo/src/pages/index.js
@@ -1,19 +1,11 @@
-import * as React from "react"
+import React from "react"
 import 'bootstrap/dist/css/bootstrap.css';
-
-function Service(props) {
-    return (
-        <div className="service">
-            <h1>{props.name}</h1>
-            <p>{props.description}</p>
-        </div>
-    )
-}
+import Service from './Service'
 
 let app = (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Prometeo e-commerce site.</p>
+    <p>Welcome to your new Prometeo e-commerce site.   Precio | Ofrecer mi producto | Hosteleros | Blog | Login |</p>
     <Service name="Tuwalu" description="build your own branding" />
     <Service name="Deliveroo" description="deliver your food" />
     <Service name="Google Analytics" description="get your analytics" />
