a5ebc3bfa7cc78e7881ae2ffc85cad0b7a04a9e0
Marvalero <mariavalerocam@gmail.com>
2021-01-31 11:10:00 +0100

    Upps

--- prometeo/src/App.js
+++ prometeo/src/App.js
@@ -4,15 +4,20 @@ import Title from './Title/Title';
 import Header from './Header/Header';
 import Mainsearch from './Mainsearch/Mainsearch';
 import Service from './Service/Service';
+import Popular from './Popular/Popular';
 
 class App extends Component {
     state = {
       services: [
-  { name: "Tuwalu", description: "build yout own branding" },
-  { name: "Deliveroo", description: "deliver your food" },
-  { name: "Google Analytics", description: "get your analytics" }
+	      { name: "Tuwalu", description: "build yout own branding", popular: true },
+	      { name: "Deliveroo", description: "deliver your food", popular: true },
+              { name: "Google Analytics", description: "get your analytics" }
       ]
     }
+
+   checkPopular(service) {
+     return service["popular"];
+   }
 
     render() {
       return (
@@ -20,6 +25,7 @@ class App extends Component {
         	<Title />
         	<Header />
         	<Mainsearch />
+        	<Popular services={this.state["services"].filter(this.checkPopular)}/>
         	{this.state["services"].map(service => {
             		return <Service name={service["name"]} description={service["description"]} />
 
---
+++ prometeo/src/Popular/Popular.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Popular extends Component {
+	render() {
+		return (
+			<div className="Popular">
+				<p>Servicios Populares</p>
+			</div>
+		)
+	}
+}
+
+export default Popular;
