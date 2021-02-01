a54b1e5d180e190fe16c2d844beb57196bbf9b91
Marvalero <mariavalerocam@gmail.com>
2021-02-01 12:05:05 +0100

    Add some companies

--- prometeo/src/App.js
+++ prometeo/src/App.js
@@ -11,7 +11,10 @@ class App extends Component {
 	      { name: "MenuCel", description: "Crea un menú digital", popular: true, url: "https://www.youtube.com/watch?v=z-InP8oZdtk" },
 	      { name: "Deliveroo", description: "Haz reparto a domicilio", popular: true, url: "https://www.youtube.com/watch?v=sSblP-U-sX4"},
 	      { name: "OpenTable", description: "Permite reservar en tu restaurante", popular: true, url: "https://www.youtube.com/watch?v=WDUDXDXekbE" },
-	      { name: "Tuwalu", description: "Personaliza tu uniforme", popular: true, url: "https://www.youtube.com/watch?v=Ltq08Z18tNI" },
+	      { name: "Gesio", description: "Permite a tus clientes pagar usando Bizum", popular: true, url: "https://www.youtube.com/watch?v=zxxQVp7Mpz8&t=70s" },
+	      { name: "Gesio", description: "Gestiona los recursos en tu empresa", popular: true, url: "https://www.youtube.com/watch?v=eaEziQey1ZU&t=70s" },
+	      { name: "Trello", description: "Gestiona objetivos economicos, de marketing y operativos", popular: true, url: "https://www.youtube.com/watch?v=cuy4qOA5htQ&t=120s" },
+	      { name: "Tuwalu", description: "Personaliza tu uniforme", popular: false, url: "https://www.youtube.com/watch?v=Ltq08Z18tNI" },
               { name: "Google Analytics", description: "get your analytics" }
       ]
     }
