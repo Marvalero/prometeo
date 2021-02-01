a181c36f4065ec8df4a82479647c0189f9a57c9e
Marvalero <mariavalerocam@gmail.com>
2021-02-01 11:49:57 +0100

    Add MenuCel company

--- prometeo/src/App.js
+++ prometeo/src/App.js
@@ -8,9 +8,10 @@ import Popular from './Popular/Popular';
 class App extends Component {
     state = {
       services: [
-	      { name: "Tuwalu", description: "Personaliza tu uniforme", popular: true, url: "https://www.youtube.com/watch?v=Ltq08Z18tNI" },
+	      { name: "MenuCel", description: "Crea un menú digital", popular: true, url: "https://www.youtube.com/watch?v=z-InP8oZdtk" },
 	      { name: "Deliveroo", description: "Haz reparto a domicilio", popular: true, url: "https://www.youtube.com/watch?v=sSblP-U-sX4"},
 	      { name: "OpenTable", description: "Permite reservar en tu restaurante", popular: true, url: "https://www.youtube.com/watch?v=WDUDXDXekbE" },
+	      { name: "Tuwalu", description: "Personaliza tu uniforme", popular: true, url: "https://www.youtube.com/watch?v=Ltq08Z18tNI" },
               { name: "Google Analytics", description: "get your analytics" }
       ]
     }
